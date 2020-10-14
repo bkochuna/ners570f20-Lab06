a811ca9e074be4cb4f3c570bf132432703245724
Brendan Kochunas <bkochuna@umich.edu>
2020-10-14 14:54:08 -0400

    Added stub routines SparseMatrix Class
    Description:
    This adds stub routines for the constructor and destructor only.
    Right now just print a message.

--- src/sparsemat.cpp
+++ src/sparsemat.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
 #include "sparsemat.hpp"
+
+using namespace std;
+using namespace SpMV;
+
+//Define the operation for the Constructor
+template <class fp_type>
+SparseMatrix<fp_type>::SparseMatrix(const size_t nrows, const size_t ncols)
+{
+  cout << "Called SparseMatrix<fp_type> constructor" << endl;
+  _nrows = nrows;
+  _ncols = ncols;
+
+}
+
+
+//Define the operation for the Destructor
+template <class fp_type>
+SparseMatrix<fp_type>::~SparseMatrix()
+{
+  cout << "Called SparseMatrix<fp_type> destructor" << endl;
+
+}
