58745a5c6bb6b8840b87cd3b794df78b5d32cd49
Brendan Kochunas <bkochuna@umich.edu>
2020-10-19 16:13:16 -0400

    WIP #47: Create subclass for testing in testSparseMatrix
    Description:
    The previous commit changed SparseMatrix to an abstract type, so
    the unit test does not compile. This commit modifies the unit test
    to create a concrete subclass of SparseMatrix so that the test
    can compile.

--- test/testSparseMatrix.cpp
+++ test/testSparseMatrix.cpp
@@ -3,12 +3,34 @@
 
 using namespace std;
 
+template <class fp_type>
+class testSpMat : public SpMV::SparseMatrix<fp_type>
+{
+    public:
+        //Define constructor to use base class constructor
+        testSpMat(const size_t nrows, const size_t ncols) :
+            SpMV::SparseMatrix<fp_type>::SparseMatrix(nrows, ncols)
+            { cout << "testSpMat::subclass constructor" << endl; }
+
+        void assembleStorage()
+            { cout << "testSpMat::no op assembly storage" << endl; }
+
+        fp_type* matVec(const fp_type* x)
+        { return NULL; }
+
+    private:
+        void _unAssemble()
+            { cout << "testSpMat::no op unassemble" << endl; }
+};
+
+
 int main() {
     cout << "Before scoping unit" << endl;
     {
 
        cout << "Before constructor" << endl;
-       SpMV::SparseMatrix<double> A(100,100);
+       //SpMV::SparseMatrix<double> A(100,100);
+       testSpMat<double> A(100,100);
        cout << "After constructor" << endl;
 
     }
