3f31c6e833f52022fcedbfd62d5754fb7e747808
Brendan Kochunas <bkochuna@umich.edu>
2020-10-19 15:50:14 -0400

    WIP #47: Creates interfaces for subclass methods of SparseMatrix
    Description:
    Define 3 pure virtual functions on the SparseMatrix base class:
     - assemblyStorage
     - matVec
     - _unAssemble
    
    These definitions make the SparseMatrix class an abstract class
    and require that the subclasses implement something. Otherwise
    the code won't compile. Presently the test does not compile.

--- src/sparsemat.hpp
+++ src/sparsemat.hpp
@@ -15,11 +15,26 @@ class SparseMatrix
         size_t _ncols = -1;
         size_t _nnz   = -1;
 
+
+        //"private" method for inverse operation of assembleStorage
+        virtual void _unAssemble()=0;
+
     public:
         //This is the constructor
         SparseMatrix(const size_t nrows, const size_t ncols);
         //This is the destructor
        ~SparseMatrix();
+
+
+        //assembleStorage is a pure virtual function.
+        //This means it must be implemented on the subclasses
+        virtual void assembleStorage()=0;
+
+
+        //Interface for matVec y<-A*x
+        //also a pure virtual function
+        virtual fp_type*  matVec(const fp_type* x)=0;
+
 
 };
 
