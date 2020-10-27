1b11497e723f8c8e87632c961d8e20f95a97e09d
Mark Lee <marklee@Marks-MacBook-Pro.local>
2020-10-27 12:28:02 +0900

    resolved merge conflict

--- src/csc/cscsparsemat.hpp
+++ src/csc/cscsparsemat.hpp
@@ -1,18 +1,4 @@
-#include <vector>
 #include <iostream>
-<<<<<<< HEAD
-
-using namespace std;
-
-template<typename T>
-class CscSparseMatrix{
-    private:
-        vector<vector<T>> csc_matrix;
-    public:
-        //Constructor - receives three inputs: element, col pointer, row idx vectors
-        //Builds CSC sparce matrix based on the input information
-        CscSparseMatrix(const vector<T> & element_vec, const vector<int> & colp_vec, const vector<int> & row_vec);
-=======
 #include "sparsemat.hpp"
 
 using namespace std;
@@ -43,14 +29,8 @@ class cscSparseMatrix : public SpMV::SparseMatrix<fp_type>
         // Csc sparse matrix destructor
         ~cscSparseMatrix()
         { cout << "cscSpMat::destructor" << endl;}
->>>>>>> 5710149469d4fd78c0926c19d673bfcd0e5c4849
 
         //Print the csc_matrix
         void printMatrix();
 
-<<<<<<< HEAD
-        //Return the csc_matrix
-        vector<vector<T>> get_csc_matrix();
-=======
->>>>>>> 5710149469d4fd78c0926c19d673bfcd0e5c4849
 };
