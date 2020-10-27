b82469c697c8b70a25ef1fda98c85f8877c9e9e1
wutingsh <wutingsh@umich.edu>
2020-10-27 17:14:22 -0400

    Added method declaration in h file; corrected typo with column; corrected return type behaviour to fit to void type, rather than returning 1, throws an exception instead. Still don't know if this works.

--- src/ell/ellpacksparsemat.cpp
+++ src/ell/ellpacksparsemat.cpp
@@ -10,7 +10,7 @@ EllpackSparseMatrix<fp_type>::EllpackSparseMatrix(const size_t nrows, const size
 {
 	cout << "Called EllpackSparseMatrix<fp_type> constructor" << endl;
 
-	_ellpackMatrix = EllpackSparseMatrix;
+	// _ellpackMatrix = EllpackSparseMatrix; // don't know what this is
 
 };
 
@@ -20,7 +20,7 @@ void EllpackSparseMatrix<fp_type>::setCoefficient(const size_t row, const size_t
 	if (row >= _nrows || col >= _ncolumns)
 	{
 		cout << "Invalid operation, row or column number exceeds size of array" << endl;
-		return 1;
+		throw "Invalid operation, row or column number exceeds size of array";
 	}
 
 	size_t sRowSize = EllpackMatrix[row].size();
@@ -37,13 +37,11 @@ void EllpackSparseMatrix<fp_type>::setCoefficient(const size_t row, const size_t
 			if (EllpackIndexMatrix[row][iI] > col)
 			{
 				EllpackMatrix[row].insert(iI, aij);
-				EllpackIndexMatrix[row].insert(iI, column);
+				EllpackIndexMatrix[row].insert(iI, col);
 				break;
 			}
 		}
 	}
-
-	return 0;
 
 };
 
--- src/ell/ellpacksparsemat.hpp
+++ src/ell/ellpacksparsemat.hpp
@@ -14,6 +14,7 @@ class EllpackSparseMatrix {
     // Constructor - receives two inputs: elements array, column index array
     // builds the ellpack
     EllpackSparseMatrix(const size_t nrows, const size_t ncols);
+    void setCoefficient(const size_t row, const size_t col, fp_type aij);
    
     // Destructor
     ~EllpackSparseMatrix()
