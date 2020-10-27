51d64b94988215d39af3e458870abcc184f3ec69
wutingsh <wutingsh@umich.edu>
2020-10-27 16:31:41 -0400

    Possibly fixed constructor statment ???

--- src/ell/ellpacksparsemat.cpp
+++ src/ell/ellpacksparsemat.cpp
@@ -6,7 +6,7 @@ using namespace SpMV;
 
 template <class fp_type>
 EllpackSparseMatrix<fp_type>::EllpackSparseMatrix(const size_t nrows, const size_t ncols, const vector <fp_type> elementVector, const vector <size_t> colIndex) :
-	SparseMatrix<T>(nrows, ncols)
+	SparseMatrix<fp_type>::SparseMatrix(nrows, ncols)
 {
 	cout << "Called EllpackSparseMatrix<fp_type> constructor" << endl;
 
