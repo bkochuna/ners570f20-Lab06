64bac2ef32452916b575e808872ad42ee4f0d8e3
Kristin Barton <knbarton@umich.edu>
2020-10-28 09:48:27 -0400

    WIP: fixed compilation errors

--- test/csrTest.cpp
+++ test/csrTest.cpp
@@ -3,52 +3,58 @@
 #include "csrsparsemat.hpp"
 
 int main() {
-  int i,j
-  // Declare 4 vec_ptrs, x and b for both float and double
-
+ 
   // Test double type
-  csrSparseMatrix<double> matD(3,3);
+  { 
+    // Declare x and b, double vec_ptrs
 
-  // Set coefficients -- this assumes indexing starts at 0
-  // 1 0 2 
-  // 3 4 0 
-  // 0 0 5 
-  matD.setCoefficient(0,0,1);
-  matD.setCoefficient(0,2,2);
-  matD.setCoefficient(1,0,3);
-  matD.setCoefficient(1,1,4);
-  matD.setCoefficient(2,2,5);
-  matD.assembleStorage();
+    // Declare sparse matrix of double type 
+    csrSparseMatrix<double> matD(3,3);  
 
-  // Set x vector to be x = (1,2,3)
+    // Set coefficients -- this assumes indexing starts at 0
+    // 1 0 2 
+    // 3 4 0 
+    // 0 0 5 
+    matD.setCoefficient(0,0,1);
+    matD.setCoefficient(0,2,2);
+    matD.setCoefficient(1,0,3);
+    matD.setCoefficient(1,1,4);
+    matD.setCoefficient(2,2,5);
+    matD.assembleStorage();
 
-  // Perform matVec multiply
+    // Set x vector to be x = (1,2,3)
 
-  // Check that b = (7,11,15), if not return error
+    // Perform matVec multiply
 
-  // Explicitly call deconstructor for test 
-  matD.~csrSparseMatrix;
+    // Check that b = (7,11,15), if not return 1
 
-  // Test float type
-  csrSparseMatrix<float> matF(3,3);
-
-  // Set coefficients -- assumes indexing starts at 0
-  // 1 2 0
-  // 3 0 4
-  // 0 5 0
-  matF.setCoefficient(0,0,1);
-  matF.setCoefficient(0,1,2);
-  matF.setCoefficient(1,0,3);
-  matF.setCoefficient(1,2,4);
-  matF.setCoefficient(2,1,5);
-  matF.assembleStorage();
-
-  // Set x vector to be x = (1,2,3)
-
-  // Perform matVec multiply
+    // Destructor called when leaving scoping unit
+  }
   
-  // Check that b = (5,15,10), if not return error
+  // Test float type
+  {
+    // Declare x and b, both float type vec_ptrs 
 
-  // Explicitly call deconstructor for test 
-  matF.~csrSparseMatrix;
+    // Declare float sparse matrix
+    csrSparseMatrix<float> matF(3,3);
+
+    // Set coefficients -- assumes indexing starts at 0
+    // 1 2 0
+    // 3 0 4
+    // 0 5 0
+    matF.setCoefficient(0,0,1);
+    matF.setCoefficient(0,1,2);
+    matF.setCoefficient(1,0,3);
+    matF.setCoefficient(1,2,4);
+    matF.setCoefficient(2,1,5);
+    matF.assembleStorage();
+
+    // Set x vector to be x = (1,2,3)
+
+    // Perform matVec multiply
+    
+    // Check that b = (5,15,10), if not return 1
+
+    // Destructor called when leaving scoping unit
+  }
 }
