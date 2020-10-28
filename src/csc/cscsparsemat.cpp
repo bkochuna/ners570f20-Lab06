0fc8abf177b9e54030800c4737321a3af689fdfd
noeyjd <71848670+noeyjd@users.noreply.github.com>
2020-10-28 04:21:19 -0400

    Update cscsparsemat.cpp

--- src/csc/cscsparsemat.cpp
+++ src/csc/cscsparsemat.cpp
@@ -2,5 +2,34 @@
 #include "cscsparsemat.hpp"
 #include "sparsemat.hpp"
 
+using namespace std;
+
 template class cscSparseMatrix<float>;
 template class cscSparseMatrix<double>;
+
+template <class fp_type>
+
+class cscSparsMatrix : public SpMV::SparsMatrix<fp_type>
+{
+    private:
+        void_unAssemble()
+        { cout << "cscSpaMat::unAssemble" << endl; }
+    
+    public:
+    
+    //Construct Matrix
+    cscSparseMatrix(const size_t nrows, const size_t ncols) :
+        SpMV::SparseMatrix<fp_type>::SparseMatrix(nrows,ncols)
+        { cout << "cscSpaMat::"=constructor" << endl; }
+        
+    //Assemble Matrix
+    void assembleStorage()
+    { cout << "cscSpaMat::matrixVector" << endl; }
+    
+    //Perform Matrix Calculation
+    
+    
+    //Deconstruct Matrix
+    ~cscSparseMatrix()
+     { cout << "cscSpaMat::destructor" << endl; }
+    
