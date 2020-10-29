aef7b5ea93a664b8795466b81dd9d15cfc640609
Eric Pearson <peareric@umich.edu>
2020-10-29 02:09:21 -0400

    Mostly syntax changes and reformatting
    Change syntax to compile with -pedantic, modify assembeStorage to not clear
    _buildCoeff, this allows a user to change the value of an element and
    reassemble without reentering all matrix values.

--- src/coo/coosparsemat.cpp
+++ src/coo/coosparsemat.cpp
@@ -12,14 +12,14 @@ CooSparseMatrix<fp_type>::CooSparseMatrix(const size_t nrows, const size_t ncols
     SparseMatrix<fp_type>::SparseMatrix(nrows, ncols)
 {
     cout << "Called CooSparseMatrix<fp_type> constructor" << endl;
-    this->state = initialized;
-};
+    this->_state = initialized;
+}
 
 // COO SparseMatrix Destructor
 template <typename fp_type>
 CooSparseMatrix<fp_type>::~CooSparseMatrix(){
     cout << "Called CooSparseMatrix<fp_type> destructor" << endl;
-};
+}
 
 
 // COO Assemble Storage
@@ -28,74 +28,84 @@ void CooSparseMatrix<fp_type>::assembleStorage(){
 
     cout << "Called COOSparseMatrix<fp_type>::assembleStorage" << endl;
 
-    if(this->_state == undefined)
-        throw runtime_error("Cannot set coefficients on undefined state!");
-    else if (this->_state == initialized)
-        throw runtime_error("Cannot set coefficients on initialized state!");
+    if( this->_state == undefined ) {
+        throw runtime_error("Cannot set coefficients on undefined state!"
+                            "Developer error, state should not be encountered.");
+    }
+    else if ( this->_state == initialized ) {
+        throw runtime_error("Cannot set coefficients on initialized state!"
+                            "First use setCoefficient() routine.");
+    }
+    else if ( this->_state == assembled ) {
+        throw runtime_error("Cannot set coefficients on assembled state!"
+                            "First use setCoefficient() routine.");
+    }
+    else { // State is building
+        for ( auto coeff : this->_buildCoeff ) {
+            row_idx.push_back(coeff.first.first);
+            col_idx.push_back(coeff.first.second);
+            coo_matrix.push_back(coeff.second);
+        }
 
-    if( this->_state == assembled )
-        this->_unAssemble();
-        return;
-
-    map<make_pair(int,int),fp_type>::iterator i;
-
-    for ( i = _buildCoeff.begin(); i != _buildCoeff.end(); i++ ){
-        this->row_idx.push_back(i->first.first);
-        this->col_idx.push_back(i->first.second);
-        this->coo_matrix.push_back(i->second);
-    };
-
-    _buildCoeff.clear();
-
-    this->_state = assembled;
+        this->_state = assembled;
+    }
     return;
-};
+}
 
 
 
 // COO unAssemble Storage
 template<typename fp_type>
 void CooSparseMatrix<fp_type>::_unAssemble(){
-    if(this->_state == undefined)
-        throw runtime_error("Cannot unset coefficients on undefined state!");
-    else if (this->_state == initialized)
-        throw runtime_error("Cannot unset coefficients on initialized state!");
-    else if (this->_state == building)
-        throw runtime_error("Cannot unset coefficients on building state!");
+    if( this->_state == undefined ) {
+        throw runtime_error("Cannot unset coefficients on undefined state!"
+                    "Developer error, state should not be encountered.");
+    }
+    else if ( this->_state == initialized ) {
+        throw runtime_error("Cannot unset coefficients on initialized state!"
+                    "Developer error, call unAssemble on assembled state");
+    }
+    else if ( this->_state == building ) {
+        throw runtime_error("Cannot unset coefficients on building state!"
+                    "Developer error, call unAssemble on assembled state");
+    }
+    else { // State is assembled
 
-    // Clear row, col and matrix vectors
-    this->row_idx.clear()
-    this->col_idx.clear()
-    this->coo_matrix.clear()
+        // Clear row, col and matrix vectors
+        row_idx.clear();
+        col_idx.clear();
+        coo_matrix.clear();
 
-    this->_state = initialized;
-
+        if ( this->_buildCoeff.empty() ) {
+            this->_state = initialized;
+        }
+        else {
+            this->_state = building;
+        }
+    }
     return;
-};
+}
 
 
 
 // COO Mat Vec
 template<typename fp_type>
-vec_ptr CooSparseMatrix<fp_type>::matVec(const vec_ptr x){
+typename CooSparseMatrix<fp_type>::vec_ptr
+CooSparseMatrix<fp_type>::matVec(const vec_ptr x) {
 
-    unsigned int size = this->coo_matrix.size();
-    unsigned int n;
+    unsigned int size = coo_matrix.size();
     int i,j;
 
-    vec_ptr<fp_type> new y(size);
-    for (n=0;n<size;n++){
-        *y[n] = 0;
+    vector<fp_type> y(size, 0);
+
+    for (unsigned int n=0; n<size; n++){
+        i=row_idx.at(n);
+        j=col_idx.at(n);
+        y.at(i) = y.at(i) + coo_matrix.at(n)*x->at(j);
     };
 
-    for (n=0;n<size;n++){
-        i=this->row_idx[n];
-        j=this->col_idx[n];
-        *y[i] = *y[i] + this->coo_matrix[n]*(*x[j]);
-    };
-
-    return y;
-};
+    return make_shared<vector<fp_type>>(y);
+}
 
 
 
--- src/coo/coosparsemat.hpp
+++ src/coo/coosparsemat.hpp
@@ -15,37 +15,26 @@ namespace SpMV {
 template<typename fp_type>
 class CooSparseMatrix : public SparseMatrix<fp_type> {
     private:
+        // Vector of elements of sparse matrix. Element at index i of
+        // coo_matrix belongs in matrix position with row number given by index
+        // i of row_idx and column number given by index i of col_idx
         vector<fp_type> coo_matrix;
-        vector<int> row_idx col_idx;
+        vector<int> row_idx, col_idx;
     public:
         // Define an alias for the gross type that templating produces
         typedef typename SparseMatrix<fp_type>::vec_ptr vec_ptr;
 
         //Constructor - receives two inputs: number of rows and numer of
         //columns, constructs parent class
-        CooSparseMatrix(const size_t nrows, const size_t ncols) :
-                        SparseMatrix<fp_type>(nrows, ncols) {};
+        CooSparseMatrix(const size_t nrows, const size_t ncols);
         
         //Deconstructor - no dynamic memory to manage, no need to implement
         //anything
-        ~CooSparseMatrix() {};
-
-        //Constructor - receives three inputs: element, col idx, row idx vectors
-        //Builds COO sparce matrix based on the input information
-        //NOTE: this constructor is not required for overall design
-        // CooSparseMatrix(const vector<fp_type> & element_vec,
-        //                 const vector<int> & col_vec,
-        //                 const vector<int> & row_vec);
-
-
-    
-        //Print the coo_matrix
-        // void printMatrix();
+        ~CooSparseMatrix();
 
         //Implementation of assembling matrix storage in COO format
         //from _buildCoeff member variable of parent class
         void assembleStorage() override;
-
 
         //Implementation of unassembling matrix storage in COO format
         //from _buildCoeff member variable of parent class
