1be554a619030756b95eec853facc85aed0ef69c
Kristin Barton <knbarton@umich.edu>
2020-10-29 13:01:49 -0400

    Add some code This adds the vectors x and b for both double and float type. It fills the vectors and performs matrix vector multiplication. Since the methods are not complete yet, the section which checks whether the result is correct has been commented out.

--- test/csrTest.cpp
+++ test/csrTest.cpp
@@ -7,7 +7,8 @@ int main() {
   // Test double type
   { 
     // Declare x and b, double vec_ptrs
-
+    auto xVec = std::make_shared<std::vector<double>>(3,0);
+    auto bVec = std::make_shared<std::vector<double>>(3,0);
     // Declare sparse matrix of double type 
     csrSparseMatrix<double> matD(3,3);  
 
@@ -23,10 +24,18 @@ int main() {
     matD.assembleStorage();
 
     // Set x vector to be x = (1,2,3)
+    xVec->at(0) = 1;
+    xVec->at(1) = 2;
+    xVec->at(2) = 3;
 
     // Perform matVec multiply
+    bVec = matD.matVec(xVec);
 
-    // Check that b = (7,11,15), if not return 1
+    // Check that b = (7,11,15), if not return error
+    // if (bVec->at(0) != 7 || bVec->at(1) != 11 || bVec->(2) != 15){
+    //   std::cout << "double matVec did not return correct values" << std::endl;
+    //   return 1;
+    // }
 
     // Destructor called when leaving scoping unit
   }
@@ -34,6 +43,8 @@ int main() {
   // Test float type
   {
     // Declare x and b, both float type vec_ptrs 
+    auto xVec = std::make_shared<std::vector<float>>(3,0);
+    auto bVec = std::make_shared<std::vector<float>>(3,0);
 
     // Declare float sparse matrix
     csrSparseMatrix<float> matF(3,3);
@@ -50,10 +61,18 @@ int main() {
     matF.assembleStorage();
 
     // Set x vector to be x = (1,2,3)
+    xVec->at(0) = 1;
+    xVec->at(1) = 2;
+    xVec->at(2) = 3;
 
     // Perform matVec multiply
+    bVec = matF.matVec(xVec);
     
     // Check that b = (5,15,10), if not return 1
+    // if (bVec->at(0) != 5 || bVec->at(1) != 15 || bVec->(2) != 10){
+    //   std::cout << "float matVec did not return correct values" << std::endl;
+    //   return 1;
+    // }
 
     // Destructor called when leaving scoping unit
   }
