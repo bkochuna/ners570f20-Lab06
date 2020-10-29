d4d27562edaa1a336fbe4073eb3c48af85e50b91
mduschenes <mduschen@umich.edu>
2020-10-29 09:13:10 -0400

    WIP Work on testing for COO for #20 Currently ctest is giving child aborted error

--- test/cooTest.cpp
+++ test/cooTest.cpp
@@ -6,14 +6,27 @@ int main() {
   cout << "Before scoping unit" << endl;
   {
     cout << "Before constructor" << endl;
-    CooSparseMatrix<double> A(100,100);
+    const unsigned int n=100;
+    CooSparseMatrix<double> A(n,n);
 
     A.setCoefficient(1,1,1);
     A.assembleStorage();
 
     A.setCoefficient(1,1,4);
+    A.setCoefficient(1,2,5);
     A.assembleStorage();
     
+    vector<double> x(n);
+    std::shared_ptr<vector<double>> y;
+    for(int i=0;i<n;i++){
+	    x.at(i) = 1;
+    };
+
+    y = A.matVec(std::make_shared<vector<double>>(x));
+
+    for(int i=0;i<n;i++){
+	    cout << "y_" << i << " : " << y->at(i) << endl;
+    };
     cout << "After constructor" << endl;
   }
 
