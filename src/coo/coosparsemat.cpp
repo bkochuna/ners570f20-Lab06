afe1309248563ceb812c0e8df1baa2f8950a045a
mduschenes <mduschen@umich.edu>
2020-10-29 00:53:23 -0400

    WIP work on COO methods for #29 and testing for #20

--- src/coo/coosparsemat.cpp
+++ src/coo/coosparsemat.cpp
@@ -6,45 +6,101 @@
 
 namespace SpMV {
 
-//Concrete implementation of the SparseMatrix abstract class using coordinate
-//list formatting for the stored sparse matrix
-template<typename T>
-class CooSparseMatrix : public SparseMatrix<T> {
-    private:
-        vector<vector<T>> coo_matrix;
-    public:
-        // Define an alias for the gross type that templating produces
-        typedef typename SparseMatrix<T>::vec_ptr vec_ptr;
-
-        //Constructor - receives two inputs: number of rows and numer of
-        //columns, constructs parent class
-        CooSparseMatrix(const size_t nrows, const size_t ncols) :
-                        SparseMatrix<T>(nrows, ncols) {};
-
-        //Constructor - receives three inputs: element, col idx, row idx vectors
-        //Builds COO sparce matrix based on the input information
-        //NOTE: this constructor is not required for overall design
-        CooSparseMatrix(const vector<T> & element_vec,
-                        const vector<int> & col_vec,
-                        const vector<int> & row_vec);
-
-        //Deconstructor - no dynamic memory to manage, no need to implement
-        //anything
-        ~CooSparseMatrix() {};
-    
-        //Print the coo_matrix
-        void printMatrix();
-    
-        //Return the coo_matrix
-        vector<vector<T>> get_coo_matrix();
-
-        //Implementation of assembling matrix storage in COO format
-        //from _buildCoeff member variable of parent class
-        void assembleStorage() override;
-
-        //Implementation of matrix vector muliplication
-        vec_ptr  matVec(const vec_ptr x) override;
+// COO SparseMatrix Constructor
+template <class fp_type>
+CooSparseMatrix<fp_type>::CooSparseMatrix(const size_t nrows, const size_t ncols) :
+    SparseMatrix<fp_type>::SparseMatrix(nrows, ncols)
+{
+    cout << "Called CooSparseMatrix<fp_type> constructor" << endl;
+    this->state = initialized;
 };
+
+// COO SparseMatrix Destructor
+template <typename fp_type>
+CooSparseMatrix<fp_type>::~CooSparseMatrix(){
+    cout << "Called CooSparseMatrix<fp_type> destructor" << endl;
+};
+
+
+// COO Assemble Storage
+template <typename fp_type>
+void CooSparseMatrix<fp_type>::assembleStorage(){
+
+    cout << "Called COOSparseMatrix<fp_type>::assembleStorage" << endl;
+
+    if(this->_state == undefined)
+        throw runtime_error("Cannot set coefficients on undefined state!");
+    else if (this->_state == initialized)
+        throw runtime_error("Cannot set coefficients on initialized state!");
+
+    if( this->_state == assembled )
+        this->_unAssemble();
+        return;
+
+    map<make_pair(int,int),fp_type>::iterator i;
+
+    for ( i = _buildCoeff.begin(); i != _buildCoeff.end(); i++ ){
+        this->row_idx.push_back(i->first.first);
+        this->col_idx.push_back(i->first.second);
+        this->coo_matrix.push_back(i->second);
+    };
+
+    _buildCoeff.clear();
+
+    this->_state = assembled;
+    return;
+};
+
+
+
+// COO unAssemble Storage
+template<typename fp_type>
+void CooSparseMatrix<fp_type>::_unAssemble(){
+    if(this->_state == undefined)
+        throw runtime_error("Cannot unset coefficients on undefined state!");
+    else if (this->_state == initialized)
+        throw runtime_error("Cannot unset coefficients on initialized state!");
+    else if (this->_state == building)
+        throw runtime_error("Cannot unset coefficients on building state!");
+
+    // Clear row, col and matrix vectors
+    this->row_idx.clear()
+    this->col_idx.clear()
+    this->coo_matrix.clear()
+
+    this->_state = initialized;
+
+    return;
+};
+
+
+
+// COO Mat Vec
+template<typename fp_type>
+vec_ptr CooSparseMatrix<fp_type>::matVec(const vec_ptr x){
+
+    unsigned int size = this->coo_matrix.size();
+    unsigned int n;
+    int i,j;
+
+    vec_ptr<fp_type> new y(size);
+    for (n=0;n<size;n++){
+        *y[n] = 0;
+    };
+
+    for (n=0;n<size;n++){
+        i=this->row_idx[n];
+        j=this->col_idx[n];
+        *y[i] = *y[i] + this->coo_matrix[n]*(*x[j]);
+    };
+
+    return y;
+};
+
+
+
+template class CooSparseMatrix<float>;
+template class CooSparseMatrix<double>;
 
 } // end SpMV naemspace
 
--- src/coo/coosparsemat.hpp
+++ src/coo/coosparsemat.hpp
@@ -9,44 +9,51 @@ using namespace std;
 
 namespace SpMV {
 
+
 //Concrete implementation of the SparseMatrix abstract class using coordinate
 //list formatting for the stored sparse matrix
-template<typename T>
-class CooSparseMatrix : public SparseMatrix<T> {
+template<typename fp_type>
+class CooSparseMatrix : public SparseMatrix<fp_type> {
     private:
-        vector<vector<T>> coo_matrix;
+        vector<fp_type> coo_matrix;
+        vector<int> row_idx col_idx;
     public:
         // Define an alias for the gross type that templating produces
-        typedef typename SparseMatrix<T>::vec_ptr vec_ptr;
+        typedef typename SparseMatrix<fp_type>::vec_ptr vec_ptr;
 
         //Constructor - receives two inputs: number of rows and numer of
         //columns, constructs parent class
         CooSparseMatrix(const size_t nrows, const size_t ncols) :
-                        SparseMatrix<T>(nrows, ncols) {};
+                        SparseMatrix<fp_type>(nrows, ncols) {};
+        
+        //Deconstructor - no dynamic memory to manage, no need to implement
+        //anything
+        ~CooSparseMatrix() {};
 
         //Constructor - receives three inputs: element, col idx, row idx vectors
         //Builds COO sparce matrix based on the input information
         //NOTE: this constructor is not required for overall design
-        CooSparseMatrix(const vector<T> & element_vec,
-                        const vector<int> & col_vec,
-                        const vector<int> & row_vec);
+        // CooSparseMatrix(const vector<fp_type> & element_vec,
+        //                 const vector<int> & col_vec,
+        //                 const vector<int> & row_vec);
 
-        //Deconstructor - no dynamic memory to manage, no need to implement
-        //anything
-        ~CooSparseMatrix() {};
+
     
         //Print the coo_matrix
-        void printMatrix();
-    
-        //Return the coo_matrix
-        vector<vector<T>> get_coo_matrix();
+        // void printMatrix();
 
         //Implementation of assembling matrix storage in COO format
         //from _buildCoeff member variable of parent class
         void assembleStorage() override;
 
+
+        //Implementation of unassembling matrix storage in COO format
+        //from _buildCoeff member variable of parent class
+        void _unAssemble() override;
+
         //Implementation of matrix vector muliplication
         vec_ptr  matVec(const vec_ptr x) override;
+
 };
 
 } // end SpMV naemspace
